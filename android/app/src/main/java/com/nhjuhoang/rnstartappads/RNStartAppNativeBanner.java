5ed88f4f197dc16ac54ec3d794d9a51c643b37f6
Nhieu Hoang <nhieunh@edoctor.vn>
2020-02-28 15:38:59 +0700

    update code

--- android/app/src/main/java/com/nhjuhoang/rnstartappads/RNStartAppNativeBanner.java
+++ android/app/src/main/java/com/nhjuhoang/rnstartappads/RNStartAppNativeBanner.java
@@ -3,7 +3,6 @@ package com.nhjuhoang.rnstartappads;
 import android.graphics.Bitmap;
 import android.util.Base64;
 import android.util.Log;
-import android.view.View;
 
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReactContext;
@@ -19,68 +18,29 @@ import com.startapp.android.publish.adsCommon.adListeners.AdEventListener;
 import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 
-public class RNStartAppNativeBanner extends ReactViewGroup {
+public class RNStartAppNativeBanner extends ReactViewGroup implements AdEventListener {
 
     public static final String EVENT_AD_RECEIVE = "onReceiveAd";
     public static final String EVENT_AD_FAILED_TO_RECEIVE = "onFailedToReceiveAd";
 
     private StartAppNativeAd startAppNativeAd;
+    private  AdEventListener adListener;
 
     public RNStartAppNativeBanner(ReactContext context) {
         super(context);
         startAppNativeAd = new StartAppNativeAd(context);
-        loadAd(this.getRootView());
+        adListener = this;
+        loadAd();
     }
 
-    private void loadAd(final View view) {
+
+    private void loadAd() {
         NativeAdPreferences nativePrefs = new NativeAdPreferences()
                 .setAdsNumber(1)
                 .setAutoBitmapDownload(true)
                 .setPrimaryImageSize(2);
-        AdEventListener adListener = new AdEventListener() {     // Callback Listener
-            @Override
-            public void onReceiveAd(Ad arg0) {
-                Log.d("ZOZOZOZOZOZOZOZOZO", "onReceiveAd");
-
-                WritableMap params = Arguments.createMap();
-                NativeAdDetails nativeAdDetails = null;
-                ArrayList<NativeAdDetails> nativeAdsList = startAppNativeAd.getNativeAds();
-                if (nativeAdsList.size() > 0) {
-                    nativeAdDetails = nativeAdsList.get(0);
-                }
-
-                // Verify that an ad was retrieved
-                if (nativeAdDetails == null) {
-                    return;
-                }
-
-                params.putString("Title", nativeAdDetails.getTitle());
-                params.putString("Description", nativeAdDetails.getDescription());
-                params.putDouble("Rating", nativeAdDetails.getRating());
-                params.putString("ImageUrl", nativeAdDetails.getImageUrl());
-                params.putString("SecondaryImageUrl", nativeAdDetails.getSecondaryImageUrl());
-                params.putString("Installs", nativeAdDetails.getInstalls());
-                params.putString("Category", nativeAdDetails.getCategory());
-                params.putString("PackacgeName", nativeAdDetails.getPackacgeName());
-                params.putString("CampaignAction", nativeAdDetails.getCampaignAction().toString());
-                params.putString("ImageBitmap", BitMapToString(nativeAdDetails.getImageBitmap()));
-                params.putString("SecondaryImageBitmap", BitMapToString(nativeAdDetails.getSecondaryImageBitmap()));
-
-                nativeAdDetails.registerViewForInteraction(view);
-
-                sendEvent(EVENT_AD_RECEIVE, params);
-            }
-
-            @Override
-            public void onFailedToReceiveAd(Ad arg0) {
-                Log.d("onFailedToReceiveAd", "ERROR ERROR ERROR ERROR ERROR ERROR ERROR");
-                WritableMap params = Arguments.createMap();
-                params.putString("message", "Error while loading Ad");
-                sendEvent(EVENT_AD_FAILED_TO_RECEIVE, params);
-            }
-        };
-
         startAppNativeAd.loadAd(nativePrefs, adListener);
+        Log.d("ZOZOZOZOZOZOZOZOZO", "======= LOAD AD NATIVE ==========");
     }
 
     public String BitMapToString(Bitmap bitmap){
@@ -103,4 +63,44 @@ public class RNStartAppNativeBanner extends ReactViewGroup {
                 .receiveEvent(this.getId(), type, event);
     }
 
+    @Override
+    public void onReceiveAd(Ad ad) {
+        Log.d("ZOZOZOZOZOZOZOZOZO", "======= EVENT_AD_RECEIVE ==========");
+
+        WritableMap params = Arguments.createMap();
+        NativeAdDetails nativeAdDetails = null;
+        ArrayList<NativeAdDetails> nativeAdsList = startAppNativeAd.getNativeAds();
+        if (nativeAdsList.size() > 0) {
+            nativeAdDetails = nativeAdsList.get(0);
+        }
+
+        // Verify that an ad was retrieved
+        if (nativeAdDetails == null) {
+            return;
+        }
+
+        params.putString("Title", nativeAdDetails.getTitle());
+        params.putString("Description", nativeAdDetails.getDescription());
+        params.putDouble("Rating", nativeAdDetails.getRating());
+        params.putString("ImageUrl", nativeAdDetails.getImageUrl());
+        params.putString("SecondaryImageUrl", nativeAdDetails.getSecondaryImageUrl());
+        params.putString("Installs", nativeAdDetails.getInstalls());
+        params.putString("Category", nativeAdDetails.getCategory());
+        params.putString("PackacgeName", nativeAdDetails.getPackacgeName());
+        params.putString("CampaignAction", nativeAdDetails.getCampaignAction().toString());
+        params.putString("ImageBitmap", BitMapToString(nativeAdDetails.getImageBitmap()));
+        params.putString("SecondaryImageBitmap", BitMapToString(nativeAdDetails.getSecondaryImageBitmap()));
+
+        nativeAdDetails.registerViewForInteraction(this.getChildAt(0));
+
+        sendEvent(EVENT_AD_RECEIVE, params);
+    }
+
+    @Override
+    public void onFailedToReceiveAd(Ad ad) {
+        Log.d("ZOZOZOZOZOZOZOZOZO", "======= EVENT_AD_FAILED_TO_RECEIVE ==========");
+        WritableMap params = Arguments.createMap();
+        params.putString("message", "EVENT_AD_FAILED_TO_RECEIVE");
+        sendEvent(EVENT_AD_FAILED_TO_RECEIVE, params);
+    }
 }
